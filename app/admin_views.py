010de578ffc7cc9e81194976d576dbd2c363bf59
haitof Simo <haitofsimo@MacBook-Pro-de-haitof.local>
2021-10-21 08:42:33 +0200

    commit

--- app/admin_views.py
+++ app/admin_views.py
@@ -1,8 +1,20 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect
 
 
 @app.route('/dash')
 def dashboard():
-    return render_template('/admin/admin_template.html')
+    return render_template('admin/admin_top.html')
+
+@app.route('/sign_up', methods= ['GET', 'POST'])
+def sign_up():
+    if request.methods == 'POST':
+        req = requests.form
+        username = req.email
+        password - req.password
+        print(username, password)
+
+    else:
+        render_template('/')
+
 
